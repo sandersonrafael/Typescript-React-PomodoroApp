9c232db3a4fbe996ef36cfd850621e1762cdeeaf
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-08-19 20:53:38 -0300

    pomodoro app finished

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useInterval from '../hooks/useInterval';
 
 import Button from './Button';
 import Timer from './Timer';
+
+import audioStart from '../sounds/src_sounds_bell-start.mp3';
+import audioFinish from '../sounds/src_sounds_bell-finish.mp3';
+
+import { secondsToTime } from '../utils/seconds-to-time';
+
+const audioStartWorking = new Audio(audioStart);
+const audioFinishWorking = new Audio(audioFinish);
 
 type Props = {
   pomodoroTimer: number;
@@ -18,28 +26,79 @@ export default function PomodoroTimer({
   cycle,
 }: Props): React.JSX.Element {
   const [mainTime, setMainTime] = useState(pomodoroTimer);
+  const [timerIsOn, setTimerIsOn] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  const [cycles, setCycles] = useState(new Array(cycle - 1).fill(true));
+  const [completedCycles, setCompletedCycles] = useState(0);
+  const [fullWorkingTime, setFullWorkingTime] = useState(0);
+  const [numberOfPomodoros, setNumberOfPomodoros] = useState(0);
+
+  const startWorking = () => {
+    setTimerIsOn(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(pomodoroTimer);
+    audioStartWorking.play();
+  };
+
+  const startResting = (long: boolean) => {
+    setTimerIsOn(true);
+    setWorking(false);
+    setResting(true);
+
+    if (long) {
+      setMainTime(longRestTime);
+    } else {
+      setMainTime(shortRestTime);
+    }
+
+    audioFinishWorking.play();
+  };
+
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+    if (resting) document.body.classList.remove('working');
+
+    if (mainTime > 0) return;
+
+    if (working && cycles.length > 0) {
+      cycles.pop();
+      startResting(false);
+    } else if (working && cycles.length <= 0) {
+      startResting(true);
+      setCycles(new Array(cycle - 1).fill(true));
+      setCompletedCycles(completedCycles + 1);
+    }
+
+    if (working) setNumberOfPomodoros(numberOfPomodoros + 1);
+    if (resting) startWorking();
+  }, [working, resting, mainTime, cycle, cycles, completedCycles, numberOfPomodoros]);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000);
+    if (working) setFullWorkingTime(time => time += 1);
+  }, timerIsOn ? 1000 : null);
 
   return (
     <div className="working pomodoro">
-      <h2>You are: working</h2>
+      <h2>Você está {!timerIsOn ? 'inativo' : working ? 'trabalhando' : 'descansando'}!</h2>
       <Timer mainTime={mainTime} />
 
       <div className="controls">
-        <Button text="Teste" />
-        <Button text="Teste" />
-        <Button text="Teste" />
+        <Button text="Start" onClick={() => startWorking()} />
+        <Button text="Rest" onClick={() => startResting(false)} />
+        <Button
+          className={!working && !resting ? 'hidden' : ''}
+          text={timerIsOn ? 'Pause' : ' Play '}
+          onClick={() => setTimerIsOn(!timerIsOn)}
+        />
       </div>
 
       <div className="details">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur libero officia porro sed numquam cum quas molestiae earum vero magni distinctio aliquam ea tempora, ut velit cumque iste nobis?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur libero officia porro sed numquam cum quas molestiae earum vero magni distinctio aliquam ea tempora, ut velit cumque iste nobis?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur libero officia porro sed numquam cum quas molestiae earum vero magni distinctio aliquam ea tempora, ut velit cumque iste nobis?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur libero officia porro sed numquam cum quas molestiae earum vero magni distinctio aliquam ea tempora, ut velit cumque iste nobis?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur libero officia porro sed numquam cum quas molestiae earum vero magni distinctio aliquam ea tempora, ut velit cumque iste nobis?</p>
+        <p>Ciclos concluídos: {completedCycles}</p>
+        <p>Horas trabalhadas: {secondsToTime(fullWorkingTime)}</p>
+        <p>Pomodoros concluídos: {numberOfPomodoros}</p>
       </div>
     </div>
   );
--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -1,5 +1,9 @@
 export function secondsToTime(seconds: number): string {
-  const min = Math.round(seconds / 60 - (seconds % 60 / 60));
   const sec = Math.round(seconds % 60);
-  return `${min>9?min:'0'+min}:${sec>9?sec:'0'+sec}`;
+  let min = Math.round(seconds / 60 - (seconds % 60 / 60));
+  const hrs = Math.round(min / 60 - (min % 60 / 60));
+
+  if (min >= 60) min = min % 60;
+
+  return `${hrs>0?hrs>9?hrs:'0'+hrs:''}${hrs?':':''}${min>9?min:'0'+min}:${sec>9?sec:'0'+sec}`;
 }
